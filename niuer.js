04e2ea42aa02af4e19535d8db2d734f5218555c5
水牛儿 <695595141@qq.com>
2013-04-26 19:59:23 +0800

    Update niuer.js
    牛耳库第二个版本：<br/>
    1、在原有的基础上增加了连缀调用。<br/>
    2、使用构造函数的方式替换直接对象表达式的方式<br/>
    3、在构造函数的原型上面增加了三个方法：css(),html(),click()。<br/>

--- niuer.js
+++ niuer.js
@@ -1,15 +1,50 @@
-/*
-*以学习的心态从基本的操作开始先封装三个获取元素节点的方法
-*至于选择器之类的东西后期慢慢优化上去
-*/
-var Base = {
-  getId : function (id) {
-		return document.getElementById(id)
-	},
-	getName : function (name) {
-		return document.getElementsByName(name)
-	},
-	getTagName : function (tag) {
-		return document.getElementsByTagName(tag);
+function Niuer() {
+	//创建一个数组，来保存获取的节点和节点数组
+	this.elements = [];
+	//通过id获取元素
+	this.getById = function (id) {
+		this.elements.push(document.getElementById(id));
+		return this;
+	};
+	//通过元素名称获取元素
+	this.getByTagName = function (tag) {
+		var tags = document.getElementsByTagName(tag);
+		for (var i = 0; i < tags.length; i ++) {
+			this.elements.push(tags[i]);
+		}
+		return this;
+	};
+	//通过name属性获取元素
+	this.getByName=function(name){
+		var tags=document.getElementsByName(name);
+		for(var i=0; i < tags.length; i ++){
+			this.elements.push(tags[i]);
+		}
+		return this;
 	}
-};
+	
+}
+//操作css的方法
+//只做了简单的css属性赋值操作
+Niuer.prototype.css = function (attr, value) {
+	for (var i = 0; i < this.elements.length; i ++) {
+		this.elements[i].style[attr] = value;
+	}
+	return this;
+}
+//操作元素内部html的方法
+//append==true是在原来基础上面添加html
+//append==false是替换原来的html
+Niuer.prototype.html = function (str,append) {
+	for (var i = 0; i < this.elements.length; i ++) {
+		append ? (this.elements[i].innerHTML += str):(this.elements[i].innerHTML = str);
+	}
+	return this;
+}
+//为选中素绑定click事件的方法
+Niuer.prototype.click = function (fn) {
+	for (var i = 0; i < this.elements.length; i ++) {
+		this.elements[i].onclick = fn;
+	}
+	return this;
+}
