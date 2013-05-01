56d1d5dc2ac1d42a53fe6742e48ab54d9e92af92
水牛儿 <695595141@qq.com>
2013-05-01 23:21:43 +0800

    Update niuer.js
    简单的封装了一下ajax方法，不过这个远远不够。后面需要把延迟加载，json，jsonp等东西加进来。

--- niuer.js
+++ niuer.js
@@ -178,6 +178,72 @@ var niuer=(function(){
 		window.onresize = fn;
 		return this;
 	}
+	
+	//简单封装ajax
+	Niuer.prototype.ajax=function(obj){
+		var xhr = createXHR();
+		obj.url = obj.url + '?rand=' + Math.random();
+		obj.data = params(obj.data);
+		if (obj.method === 'get') obj.url += obj.url.indexOf('?') == -1 ? '?' + obj.data : '&' + obj.data;
+		if (obj.async === true) {
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState == 4) {
+					callback();
+				}
+			};
+		}
+		xhr.open(obj.method, obj.url, obj.async);
+		if (obj.method === 'post') {
+			xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+			xhr.send(obj.data);	
+		} else {
+			xhr.send(null);
+		}
+		if (obj.async === false) {
+			callback();
+		}
+		function callback() {
+			if (xhr.status == 200) {
+				obj.success(xhr.responseText);			//回调传递参数
+			} else {
+				alert('获取数据错误！错误代号：' + xhr.status + '，错误信息：' + xhr.statusText);
+			}	
+		}
+		return this;
+	}
+
+	//工具函数不对外开放
+	//创建XMLHttpRequest对象的方法
+	function createXHR() {
+		if (typeof XMLHttpRequest != 'undefined') {
+			return new XMLHttpRequest();
+		} else if (typeof ActiveXObject != 'undefined') {
+			var version = [
+					'MSXML2.XMLHttp.6.0',
+					'MSXML2.XMLHttp.3.0',
+					'MSXML2.XMLHttp'
+			];
+			for (var i = 0; version.length; i ++) {
+				try {
+					return new ActiveXObject(version[i]);
+				} catch (e) {
+					//跳过
+				}	
+			}
+		} else {
+			throw new Error('您的系统或浏览器不支持XHR对象！');
+		}
+	}
+
+	//工具函数不对外开放
+	//把键值对转换为‘name=niuer&pass=niuer’形式的字符串
+	function params(data) {
+		var arr = [];
+		for (var i in data) {
+			arr.push(encodeURIComponent(i) + '=' + encodeURIComponent(data[i]));
+		}
+		return arr.join('&');
+	}
 	return function(_this){
 		return new Niuer(_this);
 	}
